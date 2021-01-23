ad56423b04152b58d5b4365b2403c3331977195f
PlayGq <77888556+PlayGq@users.noreply.github.com>
2021-01-24 01:22:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,11 +16,6 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
-    
-    @bot.command()
-async def aiueo(ctx):
-    await ctx.send('Oppai')
     
 
 
